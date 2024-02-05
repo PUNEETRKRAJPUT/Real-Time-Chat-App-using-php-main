166b4033d378a35d9a84754013160d3ebe0c0e7c
PUNEETRKRAJPUT <128739405+PUNEETRKRAJPUT@users.noreply.github.com>
2024-02-05 22:14:05 +0530

    05-2-2024

--- js/script.js
+++ js/script.js
@@ -23,24 +23,3 @@ setInterval(() => {
 
 
 // ADD Messages to the DataBase 
-window.onkeydown=(e)=>{
-  if(e.key=="Enter"){
-    update()
-  }
-}
-function update(){
-  let msg=input_msg.value;
-  input_msg.value="";
-  fetch(`addMsg.php?msg=${msg}`).then(
-    r=>{
-      if(r.ok){
-        return r.text();
-      }
-    }
-  ).then(
-    d=>{
-      console.log("msg has been added")
-      msgdiv.scrollTop=(msgdiv.scrollHeight-msgdiv.clientHeight);
-    }
-  )
-}
